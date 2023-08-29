7c638ca86e3e14e3702d68b076246d8e18658881
mary_claire_teahan <maryclaireteahan@gmail.com>
2023-08-29 15:21:56 +0100

    set up structure of game funstions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log('connected!');
+function runGame(){
+
+};
+
+function checkAnswer(){
+
+};
+
+function calculateCorrectAnswer(){
+
+};
+
+function incrementScore(){
+
+};
+
+function incrementWrongAnswer(){
+
+};
+
+function displayAdditionQuestion(){
+
+};
+
+function displaySubtractQuestion(){
+    
+};
+
+function displayMultiplyQuestion(){
+    
+};
+
+function displayDivisionQuestion(){
+    
+};
