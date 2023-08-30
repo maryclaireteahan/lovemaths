7cab82f9c7b5d52eecfc77771d09f8ff86a5e808
mary_claire_teahan <maryclaireteahan@gmail.com>
2023-08-30 14:46:00 +0100

    final tipy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+        })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter")
+            checkAnswer();
+        })
+    
+
+     runGame("addition")
 
 });
 
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
